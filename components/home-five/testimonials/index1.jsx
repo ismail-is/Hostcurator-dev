27f7af444f964b69a41d2ccf16ed6adb8f7e34b9
mscreationweb <mscreation361@gmail.com>
2025-03-19 09:02:10 +0530

    today first commit4

--- components/home-five/testimonials/index1.jsx
+++ components/home-five/testimonials/index1.jsx
@@ -63,12 +63,13 @@ function Testimonialss1() {
 			<div className="container">
 				<div className="row align-items-center">
 					{/* Left Side - Image */}
-					<div className="col-md-6 text-center" style={{height:'100%'}}>
+					<div className="col-md-6 text-center" >
 						<div className="aximo-testimonial-slider-thumb">
 							<img
 								src={testimonialsData[activeIndex].img}
 								alt="testimonial"
 								className="img-fluid"
+								style={{height:'100%'}}
                               
 							/>
 						</div>
