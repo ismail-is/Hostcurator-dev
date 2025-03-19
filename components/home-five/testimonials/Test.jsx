42a2e3d5238ed952bcf030d3c8cc50bbb4ccde16
mscreationweb <mscreation361@gmail.com>
2025-03-19 10:39:17 +0530

    demo 7

--- components/home-five/testimonials/Test.jsx
+++ components/home-five/testimonials/Test.jsx
@@ -3,32 +3,33 @@ import Image from "next/image";
 function Test({ testimonial: { author, designation, title, description, img, color, star } }) {
 	return (
 		<div  className="aximo-testimonial-slider-column"
+		id='mainbanner'
 		style={{
 		  backgroundColor: color,
-		  padding: window.innerWidth >= 640 ? "72px" : "40px", // Adjust padding based on screen size
 		  borderRadius: "29px",
-		  fontSize: window.innerWidth >= 640 ? "16px" : "14px",
 		}}>
 			<div className="aximo-testimonial-slider-data" style={{ marginBottom: '20px' }}>
 				<h3 
+				id='mainbanner1'
 					style={{ 
 						fontSize: '30px', 
 						lineHeight: '40px', 
 						color: 'white', 
 						lineHeight: '50px',
 						/* Responsive Font Size */
-						
+					
 					}}
 				>
 					{title} <img src={star} alt="Rating Stars" />
 				</h3>
 				
 				<p 
+				id='mainbanner2'
 					style={{
 						fontSize: '16px', 
 						color: 'white', 
 						textJustify: 'inter-word',
-						'@media (min-width: 640px)': { fontSize: '18px' }  /* sm */
+						// '@media (min-width: 640px)': { fontSize: '18px' }  /* sm */
 					}}
 				>
 					{description}
